64e343e8887060746cc19eee01818719a915db03
zhihaoSAR <49375487+zhihaoSAR@users.noreply.github.com>
2019-10-20 17:29:04 +0200

    empujar objeto
    cualquier cosa es de tag "Pushable" se puede empujar,para que queda bien,el rigidbody del objeto empujable hay que elegir freeze rotation x y z

--- Assets/Scripts/move.cs
+++ Assets/Scripts/move.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class move : MonoBehaviour
 {
     public float speed;
-    public CharacterController controller;
+    private CharacterController controller;
     // Start is called before the first frame update
     void Start()
     {
