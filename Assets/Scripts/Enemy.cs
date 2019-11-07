34850c582354011573a16fe8452e3cb9f4cb908e
Vidal <vidalesnar@gmail.com>
2019-11-07 01:15:07 +0100

    Inteligencia artificial básica DONE

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
@@ -8,6 +9,10 @@ public class Enemy : MonoBehaviour
     Ray ray;
     RaycastHit hit;
     GameObject obj;
+    public GameObject Target;
+    public NavMeshAgent agent;
+
+    public float distance;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,23 +22,31 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Vector3.Distance(Target.transform.position, transform.position) < distance)
+        {
+            agent.SetDestination(Target.transform.position);
+            agent.speed = 3;
+        }
+        else
+        {
+            agent.speed = 0;
+        }
     }
 
     void OnWillRenderObject()
     {
-        if (Physics.Raycast(transform.position,player.transform.position- transform.position
+        if (Physics.Raycast(transform.position, player.transform.position - transform.position
                             , out hit))
         {
             obj = hit.collider.gameObject;
 
             if (obj.tag == "Player")
             {
-                
+
                 player.recibirDano(transform.position);
             }
         }
 
-        
+
     }
 }
