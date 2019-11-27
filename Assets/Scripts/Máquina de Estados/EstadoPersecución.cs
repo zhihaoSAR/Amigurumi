fe67dabe0fed24926b7581fa48cd9c1221674aef
Vidal <vidalesnar@gmail.com>
2019-11-27 21:41:42 +0100

    Los estados van bien pero no para de perseguirte

--- Assets/Scripts/Máquina de Estados/EstadoPersecución.cs
+++ Assets/Scripts/Máquina de Estados/EstadoPersecución.cs
@@ -1,23 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EstadoPersecución : MonoBehaviour
 {
     
     private MaquinaDeEstados maquinaDeEstados;
+    private NavMeshAgent agent;
     private ControladorNavMesh controladorNavMesh;
     private ControladorVision controladorVision;
-    private AudioClip musicaPersecucionClip;
-    public AudioSource musicaPersecucion;
+    public AudioClip musicaPersecucionClip;
+    private AudioSource musicaPersecucion;
+    private float distanciaMaxima;
 
-    
     void Awake()
     {
         maquinaDeEstados = GetComponent<MaquinaDeEstados>();
         controladorNavMesh = GetComponent<ControladorNavMesh>();
         controladorVision = GetComponent<ControladorVision>();
-        //musicaPersecucion = GetComponent<AudioSource>();
+        musicaPersecucion = GetComponent<AudioSource>();
+        agent = GetComponent<NavMeshAgent>();
+    }
+    private void Start()
+    {
+        distanciaMaxima = 20f;
+
     }
 
     private void OnEnable()
@@ -27,14 +35,22 @@ public class EstadoPersecución : MonoBehaviour
 
     void Update()
     {
-        musicaPersecucion.clip = musicaPersecucionClip;
-        musicaPersecucion.Play();
         RaycastHit hit;
-        if(!controladorVision.PuedeVerAlJugador(out hit, true))
+
+        float distanciaActual = Vector3.Distance(agent.transform.position, transform.position);
+        if (distanciaActual > distanciaMaxima)
         {
+
             maquinaDeEstados.ActivarEstado(maquinaDeEstados.EstadoAlerta);
-            return;
+
         }
+
+        //if (!controladorVision.PuedeVerAlJugador(out hit, true))
+        //{
+        //    maquinaDeEstados.ActivarEstado(maquinaDeEstados.EstadoAlerta);
+        //    return;
+        //}
+
         controladorNavMesh.ActualizarPuntoDestinoNavMeshAgent();
     }
 }
