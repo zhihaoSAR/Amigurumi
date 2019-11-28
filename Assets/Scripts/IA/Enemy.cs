c3b1c164048ea86feb9c391ca69209d943546f44
Vidal <vidalesnar@gmail.com>
2019-11-28 12:16:37 +0100

    BarraCordura arreglada

---
+++ Assets/Scripts/IA/Enemy.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Enemy : MonoBehaviour
+{
+    Player player;
+    Ray ray;
+    RaycastHit hit;
+    GameObject obj;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player").GetComponent<Player>();
+    }
+
+
+    void OnWillRenderObject()
+    {
+        if (Physics.Raycast(transform.position,player.myPos.position- transform.position
+                            , out hit))
+        {
+            Debug.DrawLine(transform.position, (player.myPos.position - transform.position) * hit.distance, Color.yellow);
+            obj = hit.collider.gameObject;
+            if (obj.tag == "Player")
+            {
+                player.recibirDano(transform.position);
+            }
+        }
+
+
+    }
+}
