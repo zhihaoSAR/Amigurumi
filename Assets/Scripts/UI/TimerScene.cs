6feee6ecf99bbc12ae8c2628aad17876ae7fff30
Vidal <vidalesnar@gmail.com>
2020-01-10 20:46:35 +0100

    Escena principio introducida

---
+++ Assets/Scripts/UI/TimerScene.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TimerScene : MonoBehaviour
+{
+    [SerializeField]
+    private float delayBeforeLoading = 10f;
+
+    [SerializeField]
+    private string sceneToLoad;
+
+    private float timeElapsed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeElapsed += Time.deltaTime;
+
+        if(timeElapsed > delayBeforeLoading)
+        {
+            SceneManager.LoadScene(sceneToLoad);
+        }
+    }
+}
